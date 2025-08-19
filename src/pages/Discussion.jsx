22fd8b0cf106d387289d916106eb132c4271bebb
unknown <mangaleeshrestha@gmail.com>
2025-08-19 22:43:43 +0545

    Added comment design in discussion

--- src/pages/Discussion.jsx
+++ src/pages/Discussion.jsx
@@ -21,6 +21,12 @@ export default function Discussion() {
         questionCreator: null
 
     }
+
+    const commentSubmit = (e) => {
+        e.preventDefault();
+        alert("Comment Added");
+    }
+
     var userData = {
         name: null
     }
@@ -56,6 +62,66 @@ export default function Discussion() {
                     {
                         (questionContent.questionTags).map((tags) => { return <span key={tags} className=" flex flex-wrap bg-blue-100 min-w-[10px] w-auto text-blue-800 px-3 py-1 rounded-full text-sm" >{tags}</span> })
                     }
+                </div>
+                <hr className='my-5' />
+
+                <div className="comment-container flex flex-col gap-5">
+
+                    <div className="comments">
+                        <div className='comments-panel flex flex-row gap-7'>
+                            <span>
+                                <img className='min-w-[30px]' src={mockUsers[1].avatar} alt="" />
+                            </span>
+
+                            <div className="comment-content p-3 bg-[#e9e9e9] rounded-md flex- flex-col gap-2">
+                                <div className="username"><Link className='text-blue-600' to='/user?id=1'>Umang Shrestha</Link></div>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reprehenderit repudiandae aspernatur quas sequi voluptatem autem. Deserunt delectus eligendi cumque accusamus! Nemo eum eligendi officiis ad, aliquam consectetur ipsa repudiandae.
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reprehenderit repudiandae aspernatur quas sequi voluptatem autem. Deserunt delectus eligendi cumque accusamus! Nemo eum eligendi officiis ad, aliquam consectetur ipsa repudiandae.
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reprehenderit repudiandae aspernatur quas sequi voluptatem autem. Deserunt delectus eligendi cumque accusamus! Nemo eum eligendi officiis ad, aliquam consectetur ipsa repudiandae.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="comments">
+                        <div className='comments-panel flex flex-row gap-7'>
+                            <span>
+                                <img className='min-w-[30px]' src={mockUsers[1].avatar} alt="" />
+                            </span>
+
+                            <div className="comment-content p-3 bg-[#e9e9e9] rounded-md flex- flex-col gap-2">
+                                <div className="username"><Link className='text-blue-600' to='/user?id=1'>Umang Shrestha</Link></div>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reprehenderit repudiandae aspernatur quas sequi voluptatem autem. Deserunt delectus eligendi cumque accusamus! Nemo eum eligendi officiis ad, aliquam consectetur ipsa repudiandae.
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reprehenderit repudiandae aspernatur quas sequi voluptatem autem. Deserunt delectus eligendi cumque accusamus! Nemo eum eligendi officiis ad, aliquam consectetur ipsa repudiandae.
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reprehenderit repudiandae aspernatur quas sequi voluptatem autem. Deserunt delectus eligendi cumque accusamus! Nemo eum eligendi officiis ad, aliquam consectetur ipsa repudiandae.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="comments">
+                        <div className='comments-panel flex flex-row gap-7'>
+                            <span>
+                                <img className='min-w-[30px]' src={mockUsers[1].avatar} alt="" />
+                            </span>
+
+                            <div className="comment-content p-3 bg-[#e9e9e9] rounded-md flex- flex-col gap-2">
+                                <div className="username"><Link className='text-blue-600' to='/user?id=1'>Umang Shrestha</Link></div>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reprehenderit repudiandae aspernatur quas sequi voluptatem autem. Deserunt delectus eligendi cumque accusamus! Nemo eum eligendi officiis ad, aliquam consectetur ipsa repudiandae.
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reprehenderit repudiandae aspernatur quas sequi voluptatem autem. Deserunt delectus eligendi cumque accusamus! Nemo eum eligendi officiis ad, aliquam consectetur ipsa repudiandae.
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus reprehenderit repudiandae aspernatur quas sequi voluptatem autem. Deserunt delectus eligendi cumque accusamus! Nemo eum eligendi officiis ad, aliquam consectetur ipsa repudiandae.
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div className="add-comment my-5">
+                    <div className='comment-panel flex flex-row gap-7'>
+                        <span>
+                            <img className='w-[50px]' src={mockUsers[1].avatar} alt="" />
+                        </span>
+                        <form className='w-full' onSubmit={commentSubmit}>
+                            <input className='w-full p-3 border-2' type="text" name="" placeholder='Add a Comment...' id="" />
+                        </form>
+                    </div>
                 </div>
             </div>
         </> : <NotFound />
